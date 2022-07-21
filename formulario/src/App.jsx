f4e36e21bc838d535f3f01e7af08aad4db7e19ec
mayconinforgames2014 <maycon_gus2009@hotmail.com>
2022-07-21 20:09:37 -0300

    finalizando formulario

--- formulario/src/App.jsx
+++ formulario/src/App.jsx
@@ -26,6 +26,90 @@ function App() {
       >
         Formulário
       </Center>
+      <Flex
+        align="center"
+        justify="center"
+        bg="blackAlpha.200"
+        h="calc(100vh - 150px)"      
+      >
+        <Center
+          w="100%"
+          maxW={840}
+          bg="white"
+          top={100}
+          position="absolute"
+          borderRadius={5}
+          p="6"
+          boxShadow="0 1px 2px #ccc"
+        >
+          <FormControl display="flex" flexDir="column" gap="4" >
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor='nome'>Nome Completo</FormLabel>
+                <Input id='nome'/>
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor='email'>E-mail</FormLabel>
+                <Input id='email' type="email" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor='nasc'>Data de Nascimento</FormLabel>
+                <Input id='nasc' type="date" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor='naturak'>Naturalidade</FormLabel>
+                <Input id='natural' />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor='cel'>Celular</FormLabel>
+                <Input id='cel' type="number" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor='telefone'>Telefone</FormLabel>
+                <Input id='telefone' type="number" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor='endereco'>Endereco</FormLabel>
+                <Input id='endereco'/>
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor='cidade'>Cidade</FormLabel>
+                <Input id='cidade' />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel>Sexo</FormLabel>
+                  <HStack spacing="24px">
+                    <Radio value="Masculino">Masculino</Radio>
+                    <Radio value="Masculino">Feminino</Radio>
+                  </HStack>  
+              </Box>
+            </HStack>
+            <HStack justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="teal.600"
+                color="white"
+                fontWeight="bold"
+                fontSize="xl"
+                mt="2"
+                _hover={{ bg: "teal.800" }}
+              >
+                Enviar
+              </Button>
+            </HStack>
+          </FormControl>
+        </Center>
+      </Flex>
     </Box>
   )
 
